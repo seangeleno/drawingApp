e5b9b7dded3a9e8f480bb01b7f61cbde6f2a106b
Sean Esteva <seangeleno@gmail.com>
2016-02-06 04:52:45 -0800

    mouse tracking on x and y coordinates using getBoundingClient().left+top

--- logic.js
+++ logic.js
@@ -1,16 +1,32 @@
 var canvas,
     context;
+//getBoundingClientRect
+//The returned value is a DOMRect object, which contains read-only left, top, right and
+//bottom properties describing the border-box in pixels. top and left are relative to the top-left of the viewport.
 
+// rect is a DOMRect object with six properties: left, top, right, bottom, width, height
+//var rect = obj.getBoundingClientRect();
+
+//define mouse coordinates
+function getCanvasCoordinates(event){
+  var x = event.clientX - canvas.getBoundingClientRect().left;
+  var y = event.clientY - canvas.getBoundingClientRect().top;
+
+  return {x: x, y:y};
+}
 
 //define dragstart, drag and dragStop
 function dragStart(event) {
   console.log("Drag Start");
+  console.log(getCanvasCoordinates(event));
 }
 function drag(event) {
   console.log('mouse move is working');
+  console.log(getCanvasCoordinates(event));
 }
 function dragStop(event) {
   console.log('dragStop');
+  console.log(getCanvasCoordinates(event));
 }
 
 //function invoked when document is fully loaded
