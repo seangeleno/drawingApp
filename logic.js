858eb3330e33c3a4384efbba17580c56597826f5
Sean Esteva <seangeleno@gmail.com>
2016-02-06 05:11:47 -0800

    added dragStart and stop function logic - RADAR glitch

--- logic.js
+++ logic.js
@@ -1,5 +1,8 @@
 var canvas,
-    context;
+    context,
+    dragging = false, //this will be dragging if mouse move is followed by mouse down
+    dragStartLocation;
+
 //getBoundingClientRect
 //The returned value is a DOMRect object, which contains read-only left, top, right and
 //bottom properties describing the border-box in pixels. top and left are relative to the top-left of the viewport.
@@ -12,21 +15,47 @@ function getCanvasCoordinates(event){
   var x = event.clientX - canvas.getBoundingClientRect().left;
   var y = event.clientY - canvas.getBoundingClientRect().top;
 
+  //return object where x is x and y is y
   return {x: x, y:y};
+}
+
+function drawLine(position){
+  context.beginPath();
+  context.moveTo(dragStartLocation.x, dragStartLocation.y);//this will be the first point and during mouse up the line will be drawn
+  context.lineTo(position.x, position.y); //current position of x and y during mouseMove event
+  context.stroke(); // The stroke() method actually draws the path you have defined with all those moveTo() and lineTo() methods. So Cool!
 }
 
 //define dragstart, drag and dragStop
 function dragStart(event) {
-  console.log("Drag Start");
-  console.log(getCanvasCoordinates(event));
+  // console.log("Drag Start");
+  // console.log(getCanvasCoordinates(event));
+
+  dragging = true;
+  dragStartLocation = getCanvasCoordinates(event);
+
 }
 function drag(event) {
-  console.log('mouse move is working');
-  console.log(getCanvasCoordinates(event));
-}
+//   console.log('mouse move is working');
+//   console.log(getCanvasCoordinates(event));
+
+  var position;
+  if (dragging === true) {
+    position = getCanvasCoordinates(event);
+    //pass position into drawLine
+    drawLine(position);
+  }
+
+ }
 function dragStop(event) {
-  console.log('dragStop');
-  console.log(getCanvasCoordinates(event));
+  // console.log('dragStop');
+  // console.log(getCanvasCoordinates(event));
+
+  dragging = false; //dragging stops here
+  var position = getCanvasCoordinates(event);
+  drawLine(position);
+
+
 }
 
 //function invoked when document is fully loaded
@@ -36,7 +65,8 @@ function init(){
   context.strokeStyle = 'rebeccapurple';
   context.lineWidth = 6;
   context.lineCap = 'square';
-  //mouseup,down, drag
+
+  //mouseup,down, drag - will be used in functions above
   canvas.addEventListener('mousedown', dragStart, false);
   canvas.addEventListener('mousemove', drag, false);
   canvas.addEventListener('mouseup', dragStop, false);
